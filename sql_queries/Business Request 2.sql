b9739b54849e6ff16a801eed3a914c98f12c1bd9
Abubakar Iyanda <profabubakar2820@gmail.com>
2025-01-11 11:00:39 +0100

    Update Business Request 2.sql

--- sql_queries/Business Request 2.sql
+++ sql_queries/Business Request 2.sql
@@ -1,3 +1,9 @@
+-- Generate a report that evaluates the target performance for trips at the monthly and city level. 
+-- For each city and month, compare the actual total trips with the target trips and categorise the performance as follows:
+-- If actual trips are greater than target trips, mark it as "Above Target".
+-- If actual trips are less than or equal to target trips, mark it as "Below Target".
+
+
 WITH Target AS (
     SELECT 
         city_id,
